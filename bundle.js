c2106667c68047876ea822158a2331f97ff011ef
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-06 17:48:24 +0100

    Title and image showing on page:

--- bundle.js
+++ bundle.js
@@ -79,9 +79,22 @@
             const articleEl = document.createElement("div");
             articleEl.className = "article";
             articleEl.textContent = article;
-            articleEl.innerText = article.innerText;
+            articleEl.innerText = article.title;
+            this.imageElement(article);
             this.mainContainerEl.append(articleEl);
           });
+        }
+        imageElement(article) {
+          const articleImg = document.createElement("img");
+          articleImg.src = article.image;
+          articleImg.alt = "Image of news article";
+          this.mainContainerEl.append(articleImg);
+        }
+        urlElement(article) {
+          const articleUrl = document.createElement("a");
+          articleUrl.href = article.url;
+          articlarticle.Url.textContent = "News Article URL";
+          this.mainContainerEl.append(articleUrl);
         }
       };
       module.exports = NewsView2;
--- newsView.js
+++ newsView.js
@@ -17,9 +17,24 @@ class NewsView {
       const articleEl = document.createElement('div');
       articleEl.className = 'article';
       articleEl.textContent = article;
-      articleEl.innerText = article.innerText;
+      articleEl.innerText = article.title;
+      this.imageElement(article);
       this.mainContainerEl.append(articleEl);
     })
+  }
+
+  imageElement(article) {
+    const articleImg = document.createElement('img');
+    articleImg.src = article.image;
+    articleImg.alt = 'Image of news article';
+    this.mainContainerEl.append(articleImg);
+  }
+
+  urlElement(article) {
+    const articleUrl = document.createElement('a');
+    articleUrl.href = article.url
+    articlarticle.Url.textContent = 'News Article URL';
+    this.mainContainerEl.append(articleUrl);
   }
 
 }
